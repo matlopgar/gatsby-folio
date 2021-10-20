720f981139d6bc29c058dbdcb6b203c987fdf82d
Matheus Garcia <matheuslopesgarcia@gmail.com>
2021-10-19 22:35:50 -0300

    configs and plugins

---
+++ config/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Portfolio",
+        social: [
+            {
+                name: 'github',
+                url: 'https://github.com/matlopgar'
+            }
+        ]
+    }
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,41 @@
+const siteMetadata = require('./config/metadata')
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "folio",
-  },
-  plugins: [],
+  siteMetadata,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/portfolio.png`
+      },
+    },
+
+    `gatsby-plugin-react-helmet`,
+
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    
+    `gatsby-plugin-offline`,
+    
+  ]
 };
